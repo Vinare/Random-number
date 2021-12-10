e9bc3c5fc3706e56023c535cf8d2e4e17603e833
Vinare <83562166+Vinare@users.noreply.github.com>
2021-12-10 13:06:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ let randomNum = Math.floor(Math.random() * 100) + 1
 
 function guessNumber() {
   let userNum = +prompt('Угадай число от 1 до 100') 
-  while (isNaN(userNum) || userNum === "") {
+  if (isNaN(userNum) || userNum === "") {
     alert('Можно вводить только числа')
   if (userNum > randomNum) {
     alert('Загаданное число меньше')
+    userNum = guessNumber();
+    
   } else if (userNum < randomNum) {
     alert('Загаданное число больше')
   } else if (userNum === randomNum) {
     alert('Поздравляю, Вы угадали!!!')
+    return userNum;
   } else if (userNum === 0) {
     alert('Нужно угадать число от 1 до 100')
   } else if (userNum == null) {
     alert('Игра окончена')
-    break
+    return 'Игра окончена';
   }
 }
-  return guessNumber()
+
 }
 console.log(guessNumber()) 
 
