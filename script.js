79dbf9d44c5e174eebd91902968de18b670b9059
Vinare <scrat-me@mail.ru>
2021-12-12 21:58:19 +0300

    My first super bot done

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 'use strict'
 
-let attempts = 5;
+let attempts = 10;
 
 function guessNumberGlobal() {
 let randomNum = Math.floor(Math.random() * 100) + 1
 
-function guessNumber(attemptsIndex) {
-  let userNum = prompt('Угадай число от 1 до 100') 
+  function guessNumber(attemptsIndex) {
+    let userNum = prompt('Угадай число от 1 до 100') 
 
-  if (isNaN(userNum) || userNum == "") {
-    alert('Можно вводить только числа')
-    guessNumber()
-  
-  } if (userNum == null) {
-    alert('Игра окончена')
-  
-  } if (!isNaN(parseFloat(userNum)) && isFinite(userNum)) {
-    userNum = +userNum
+    if (isNaN(userNum) || userNum == "") {
+      alert('Можно вводить только числа')
+      guessNumber()
+    
+    } else if (userNum == null) {
+      alert('Игра окончена')
+    
+    } else if (!isNaN(parseFloat(userNum)) && isFinite(userNum)) {
+      userNum = +userNum
 
-    if (attemptsIndex > 1) {
-  
-    if (userNum > randomNum) {
-    attemptsIndex--;
-    alert('Загаданное число меньше. У Вас осталось ' + attemptsIndex + ' попыток')
-    guessNumber(attemptsIndex)  
-  
-  } if (userNum < randomNum) {
-    attemptsIndex--;
-    alert('Загаданное число больше. У Вас осталось ' + attemptsIndex + ' попыток')
-    guessNumber(attemptsIndex)
-  } 
-   if (userNum === randomNum) {
-    let con = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
-  
-   if (con == true) { 
-   guessNumberGlobal()
-  } else {
-    alert('Игра окончена')
-  }
-  }
-  } if (attemptsIndex == 1) { 
-    let con = confirm('Попытки закончились, хотите сыграть еще?')
-
+      if (attemptsIndex > 1) {
+    
+      if (userNum > randomNum) {
+      attemptsIndex--;
+      alert('Загаданное число меньше. У Вас осталось ' + attemptsIndex + ' попыток')
+      guessNumber(attemptsIndex)  
+    
+    } else if (userNum < randomNum) {
+      attemptsIndex--;
+      alert('Загаданное число больше. У Вас осталось ' + attemptsIndex + ' попыток')
+      guessNumber(attemptsIndex)
+    
+    } else if (userNum === randomNum) {
+      let con = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
+    
     if (con == true) { 
-    guessNumberGlobal() 
-  } else {
-    alert('Игра окончена')
+    guessNumberGlobal()
+    } else {
+      alert('Игра окончена')
+    }
+    }
+    } else { 
+      let con = confirm('Попытки закончились, хотите сыграть еще?')
+
+      if (con == true) { 
+      guessNumberGlobal() 
+    } else {
+      alert('Игра окончена')
+    }
+    }
   }
   }
-}
-}
 console.log(guessNumber(attempts)) 
 }
 console.log(guessNumberGlobal())
