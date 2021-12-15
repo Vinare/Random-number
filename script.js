0e56f8ecc8387d9d54cc7a5111bb6f6729b938d0
Vinare <scrat-me@mail.ru>
2021-12-15 15:41:50 +0300

    Finished

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
 'use strict';
 
-const attempts = 10;
-
-function guessNumberGlobal(attemptsIndex) {
+function guessNumberGlobal() {
 let randomNum;
 const attempts = 10;
-  function newRound() {
-   randomNum = Math.floor(Math.random() * 100) + 1;
-   attemptsIndex = attempts;
-   return guessNumber();
-  }
+let attemptsIndex = attempts;
+    
+    function newRound() {
+    randomNum = Math.floor(Math.random() * 100) + 1;
+    attemptsIndex = attempts;
+    return guessNumber();
+    }
 
-  function guessNumber() {
-    let userNum = prompt('Угадай число от 1 до 100'); 
-    
-    if (userNum === null) {
-      alert('Игра окончена');
-      return 'Игра окончена';
-    } 
-    
-    if (+userNum === randomNum) {
-      let con = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-      con === true ? newRound() : alert('Игра окончена'); 
+    function guessNumber() {
+      let userNum = prompt('Угадай число от 1 до 100'); 
+      
+      if (userNum === null) {
+        alert('Игра окончена');
+        return 'Игра окончена';
+      } 
+      
+      if (+userNum === randomNum) {
+        let con = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+        con === true ? newRound() : alert('Игра окончена'); 
+        return userNum;
+      }
+
+      if (attemptsIndex > 1) {
+      
+        switch (true) {
+        case !userNum.trim() || isNaN(userNum):
+          alert('Можно вводить только числа'); break;
+        case +userNum > randomNum :
+          attemptsIndex--;
+          alert('Загаданное число меньше. У Вас осталось ' + attemptsIndex + ' попыток'); break; 
+        case +userNum < randomNum :
+          attemptsIndex--;
+          alert('Загаданное число больше. У Вас осталось ' + attemptsIndex + ' попыток'); break;
+      } 
+      } else { 
+        let con = confirm('Попытки закончились, хотите сыграть еще?');
+
+        con === true ? newRound() : alert('Игра окончена');
+        return userNum;
+        }
+
+      userNum = guessNumber();
       return userNum;
     }
+    return newRound;
+}   
 
-    if (attemptsIndex > 1) {
-    
-    switch (true) {
-    case !userNum.trim() || isNaN(userNum):
-      alert('Можно вводить только числа'); break;
-    case +userNum > randomNum :
-      attemptsIndex--;
-      alert('Загаданное число меньше. У Вас осталось ' + attemptsIndex + ' попыток'); break; 
-    case +userNum < randomNum :
-      attemptsIndex--;
-      alert('Загаданное число больше. У Вас осталось ' + attemptsIndex + ' попыток'); break;
-    } 
-    } else { 
-    let con = confirm('Попытки закончились, хотите сыграть еще?');
-
-    con === true ? newRound() : alert('Игра окончена');
-    return userNum;
-    }
-
-    userNum = guessNumber();
-    return userNum;
-  }
-  const round = newRound();
-  return round;
-  
-} 
-
-const play = guessNumberGlobal(attempts);
+const play = guessNumberGlobal();
 
 console.log(play()); 
 
