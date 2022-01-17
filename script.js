5b65a92ea3e63002a5a887f23da8537a0869ca35
Vinare <scrat-me@mail.ru>
2022-01-17 22:14:45 +0300

    Refactoring

--- script.js
+++ script.js
@@ -27,14 +27,14 @@ const attempts = attemptsIndex;
       if (attemptsIndex > 1) {
       
         switch (true) {
-        case !userNum.trim() || isNaN(userNum):
-          alert('Можно вводить только числа'); break;
-        case +userNum > randomNum :
-          attemptsIndex--;
-          alert('Загаданное число меньше. У Вас осталось ' + attemptsIndex + ' попыток'); break; 
-        case +userNum < randomNum :
-          attemptsIndex--;
-          alert('Загаданное число больше. У Вас осталось ' + attemptsIndex + ' попыток'); break;
+          case !userNum.trim() || isNaN(userNum):
+            alert('Можно вводить только числа'); break;
+          case +userNum > randomNum :
+            attemptsIndex--;
+            alert('Загаданное число меньше. У Вас осталось ' + attemptsIndex + ' попыток'); break; 
+          case +userNum < randomNum :
+            attemptsIndex--;
+            alert('Загаданное число больше. У Вас осталось ' + attemptsIndex + ' попыток'); break;
         } 
       } else { 
         confirm('Попытки закончились, хотите сыграть еще?') ? 
@@ -43,7 +43,6 @@ const attempts = attemptsIndex;
       }
 
       userNum = guessNumber();
-      
       return userNum;
     }
     return newRound;
